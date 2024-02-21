2455ef297b25d9e171f8ecc5ecee433b70419202
alinur1509 <86433012+alinur1509@users.noreply.github.com>
2024-02-21 19:39:02 +0600

    commit

---
+++ lab 4/README.md
@@ -0,0 +1,3 @@
+# angular-p6fytf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-p6fytf-srinfv)
---
+++ lab 4/src/app/products.ts
@@ -0,0 +1,166 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  kaspilink: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'iPhone 11 128Gb',
+    price: 650,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: черный\n' +
+      'тип экрана: сенсорный мультитач цветной IPS, Liquid Retina HD\n',
+    rating: 5.0,
+    //arrayofimages: ['123','234234','534534'],
+    imageSrc:
+      'https://https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h17/h14/31489167425566/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg.media-amazon.com/images/I/71Ujb4adTRL._AC_SL1500_.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item',
+  },
+  {
+    id: 2,
+    name: 'iPhone 14 Pro Max 256Gb',
+    price: 1490,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: фиолетовый\n' +
+      'тип экрана: OLED, Super Retina XDR display с возможностью постоянной работы\n',
+    rating: 5.0,
+    //arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h00/h18/62948780834846/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg ',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+  },
+  {
+    id: 3,
+    name: 'iPhone 14 128Gb ',
+    price: 940,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: черный\n' +
+      'тип экрана: OLED, Super Retina XDR display\n',
+    rating: 5.0,
+    //arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8f/h61/63072540131358/apple-iphone-14-128gb-cernyj-106363023-1.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-14-128gb-chernyi-106363023/?c=750000000#!/item',
+  },
+  {
+    id: 4,
+    name: 'iPhone 13 128Gb ',
+    price: 830,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: белый\n' +
+      'тип экрана: OLED, Super Retina XDR\n',
+    rating: 4.2,
+    //arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h17/h14/31489167425566/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg.media-amazon.com/images/I/71Ujb4adTRL._AC_SL1500_.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-13-128gb-belyi-102298420/?c=750000000#!/item',
+  },
+  {
+    id: 5,
+    name: 'iPhone 13 128Gb ',
+    price: 850,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: синий\n' +
+      'тип экрана: OLED, Super Retina XDR\n',
+    rating: 4.2,
+    //arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h91/h1d/46392661737502/apple-iphone-13-128gb-sinij-102298364-1-Container.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-13-128gb-sinii-102298364/?c=750000000#!/item',
+  },
+  {
+    id: 6,
+    name: 'iPhone 14 Pro 256Gb',
+    price: 1400,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: фиолетовый\n' +
+      'тип экрана: OLED, Super Retina XDR display с возможностью постоянной работы\n',
+    rating: 5.0,
+    //arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4d/h62/62948396728350/apple-iphone-14-pro-128gb-fioletovyj-106363319-1.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-14-pro-256gb-fioletovyi-106363319/?c=750000000#!/item',
+  },
+  {
+    id: 7,
+    name: 'iPhone 11 64Gb ',
+    price: 580,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: черный\n' +
+      'тип экрана: сенсорный мультитач цветной IPS, Liquid Retina HD\n',
+    rating: 5.0,
+    //arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h25/hfa/32690571706398/apple-iphone-11-64gb-slim-box-cernyj-100692387-1-Container.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-11-64gb-slim-box-chernyi-100692387/?c=750000000#!/item',
+  },
+  {
+    id: 8,
+    name: 'iPhone 11 128Gb',
+    price: 660,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: белый\n' +
+      'тип экрана: сенсорный мультитач цветной IPS, Liquid Retina HD\n',
+    rating:  5.0,
+    //arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3a/hb4/33208264228894/apple-iphone-11-128gb-slim-box-belyj-100692385-1-Container.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-belyi-100692385/?c=750000000#!/item',
+  },
+  {
+    id: 9,
+    name: 'iPhone 14 128Gb ',
+    price: 930,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: черный\n' +
+      'тип экрана: сенсорный мультитач цветной IPS, Liquid Retina HD\n',
+    rating: 4.2,
+    //arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8b/h41/63073057177630/apple-iphone-14-128gb-goluboj-106363150-1.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000#!/item',
+  },
+  {
+    id: 1,
+    name: 'Phone 11',
+    price: 650,
+    description:
+      'технология NFC: Да\n' +
+      'цвет: черный\n' +
+      'тип экрана: сенсорный мультитач цветной IPS, Liquid Retina HD\n',
+    rating: 4.2,
+   // arrayofimages: ['123', '234234', '534534'],
+    imageSrc:
+      'https://https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h17/h14/31489167425566/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg.media-amazon.com/images/I/71Ujb4adTRL._AC_SL1500_.jpg',
+    kaspilink:
+      'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
